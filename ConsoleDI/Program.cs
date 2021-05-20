facd3f6909fe86bcf7f1cb841dffeb04b46112a3
RobinAxelsson <axelsson.robin@outlook.com>
2021-05-20 11:37:04 +0200

    Dependency without lifetime

--- ConsoleDI/Program.cs
+++ ConsoleDI/Program.cs
@@ -1,12 +1,79 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Runtime.Intrinsics.Arm;
 
 namespace ConsoleDI
 {
-    class Program
+    //https://youtu.be/NkTF_6IQPiY?t=599
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var ctorParams = typeof(ServiceConsumer).GetConstructors()
+                .Select(x => x.GetParameters());
+            var container = new DependencyContainer();
+            container.AddDependency<HelloService>();
+            container.AddDependency<ServiceConsumer>();
+            container.AddDependency<MessageService>();
+
+            var resolver = new DependencyResolver(container);
+            var service1 = resolver.GetService<ServiceConsumer>();
+            var service2 = resolver.GetService<ServiceConsumer>();
+            var service3 = resolver.GetService<ServiceConsumer>();
+
+            service1.Print();
+            service2.Print();
+            service3.Print();
+
         }
     }
+    public class Dependency
+    {
+        public Type Type { get; set; }
+        public DependencyLifetime Lifetime { get; set; }
+    }
+    public class DependencyResolver
+    {
+        private DependencyContainer _container;
+        public DependencyResolver(DependencyContainer container)
+        {
+            _container = container;
+        }
+
+        private object GetService(Type type)
+        {
+            var dependency = _container.GetDependency(type);
+            var constructor = dependency.GetConstructors().Single();
+            var parameters = constructor.GetParameters();
+            var parameterImplemenatations = new object[parameters.Length];
+
+            if (parameters.Length > 0)
+            {
+                for (int i = 0; i < parameters.Length; i++)
+                {
+                    parameterImplemenatations[i] = GetService(parameters[i].ParameterType);
+                }
+
+                return Activator.CreateInstance(dependency, parameterImplemenatations);
+            }
+
+            return Activator.CreateInstance(dependency);
+        }
+
+        public T GetService<T>()
+        {
+            return (T) GetService(typeof(T));
+        }
+    }
+    public class DependencyContainer
+    {
+        private readonly List<Type> _dependencies = new();
+        public void AddDependency(Type type) => _dependencies.Add(type);
+        public void AddDependency<T>() => _dependencies.Add(typeof(T));
+        public Type GetDependency(Type type) => _dependencies.First(x => x.Name == type.Name);
+    } 
+    
 }
---
+++ ConsoleDI/Services.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDI
+{
+    public enum DependencyLifetime
+    {
+        Singleton = 0,
+        Transient = 1
+    }
+    public class ServiceConsumer
+    {
+        private HelloService _hello;
+
+        public ServiceConsumer(HelloService hello)
+        {
+            _hello = hello;
+        }
+        public void Print() => _hello.Print();
+    }
+
+    public class HelloService
+    {
+        private MessageService _message;
+        public HelloService(MessageService message)
+        {
+            _message = message;
+        }
+        public void Print()
+        {
+            System.Diagnostics.Debug.WriteLine("Hello World : " + _message.Message());
+        }
+    }
+
+    public class MessageService
+    {
+        private int _random;
+
+        public MessageService()
+        {
+            _random = new Random().Next(1000);
+        }
+        public string Message()
+        {
+            return $"Yo #{_random}";
+        }
+    }
+    public interface ITest
+    {
+        public void Print();
+    }
+
+    public class Test : ITest
+    {
+        public Test()
+        {
+            Print();
+        }
+        public void Print()
+        {
+            System.Diagnostics.Debug.WriteLine("Hello from test");
+        }
+    }
+}
